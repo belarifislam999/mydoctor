3175f137cafcc519d97b6127b6b2427e94e9b0db
belarifislam999 <belarif.islam999@gmail.com>
2026-03-28 15:23:02 +0100

    force migrate all

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -e
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --fake-initial
+python manage.py migrate --run-syncdb
+python manage.py migrate accounts
+python manage.py migrate appointments
 python manage.py migrate
 python manage.py createsuperadmin
